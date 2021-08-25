abb676f4ea55bdcc08af3219bd138257ec57b326
Alex Kaplun <you@example.com>
2021-08-24 22:13:03 -0400

    hw62

--- Javascript/62/hw62.js
+++ Javascript/62/hw62.js
@@ -14,4 +14,28 @@
     console.log(getDayName(1));
     console.log(getDayIndex('Wednesday'));
 
+
+    ////////////////////
+
+
+    let yrs;
+    let rate;
+
+    function setRate(r) {
+        r = rate;
+    }
+
+    function setYears(y) {
+        y = yrs;
+    }
+
+    function calculateInterest(amount) {
+        let interest = (amount * rate) * yrs;
+        return interest;
+    }
+
+    setYears(5);
+    setRate('.01');
+    console.log(calculateInterest(1000));
+
 }());
